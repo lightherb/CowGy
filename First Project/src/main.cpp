58d24fa33e9719a2c95ad5ae98675d8e42cc5f37
lightherb <light.herb@gmail.com>
2021-03-16 20:22:53 +0700

    test gyro

--- First Project/src/main.cpp
+++ First Project/src/main.cpp
@@ -1,9 +1,36 @@
 #include <Arduino.h>
+#include <Arduino_LSM9DS1.h>
 
 void setup() {
   // put your setup code here, to run once:
+  Serial.begin(9600);
+  while (!Serial);
+  Serial.println("Started");
+
+  if (!IMU.begin()) {
+    Serial.println("Failed to initialize IMU!");
+    while (1);
+  }
+
+  Serial.print("Accelerometer sample rate = ");
+  Serial.print(IMU.accelerationSampleRate());
+  Serial.println(" Hz");
+  Serial.println();
+  Serial.println("Acceleration in G's");
+  Serial.println("X\tY\tZ");
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
+  float x, y, z;
+
+  if (IMU.accelerationAvailable()) {
+    IMU.readAcceleration(x, y, z);
+
+    Serial.print(x);
+    Serial.print('\t');
+    Serial.print(y);
+    Serial.print('\t');
+    Serial.println(z);
+  }
 }
--- Second Project/src/main.cpp
+++ Second Project/src/main.cpp
@@ -3,8 +3,11 @@
 void setup() {
   // put your setup code here, to run once:
   // Second Project
+  Serial.begin(9600);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
+  Serial.println("Started");
+  delay(1000);
 }
