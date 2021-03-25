e4f14a1f1f8f5cbea8756dc62529651d8ace458d
lightherb <light.herb@gmail.com>
2021-03-25 23:01:26 +0700

    Add Heltec

--- Test/src/main.cpp
+++ Test/src/main.cpp
@@ -2,8 +2,15 @@
 
 void setup() {
   // put your setup code here, to run once:
+  // initialize digital pin LED_BUILTIN as an output.
+  pinMode(LED_BUILTIN, OUTPUT);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
+  digitalWrite(LED_BUILTIN, HIGH);   // turn the LED on (HIGH is the voltage level)
+  delay(1000);                       // wait for a second
+  digitalWrite(LED_BUILTIN, LOW);    // turn the LED off by making the voltage LOW
+  delay(1000);                       // wait for a second
+
 }
